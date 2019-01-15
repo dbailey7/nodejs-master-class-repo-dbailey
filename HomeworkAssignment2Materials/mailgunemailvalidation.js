cfdb45ff8294837049074241bf56a37843d4ca89
dbailey7 <david.bailey@technologist.com>
2019-01-15 15:35:35 -0500

    Create mailgunemailvalidation.js
    This is a test module for the mailgun.com email validation API.

---
+++ HomeworkAssignment2Materials/mailgunemailvalidation.js
@@ -0,0 +1,18 @@
+/*
+ * This is a test module for the mailgun.com email validation API.  Enjoy watching!
+ */
+
+var DOMAIN = 'YOUR-DOMAIN';
+var mailgun = require('mailgun-js')({
+  apiKey: "YOUR-API-KEY",
+  domain: DOMAIN
+});
+
+mailgun.validate('YOUR-VERIFIED_RECIPIENT-EMAIL', function (error, body) {
+  if(!error, body){
+    console.log('Success: domain ', sku.metadata.item_description, ' was validated okay.');
+    console.log('Body is ', body);
+  } else {
+    console.log('Error: domain validation failed; error was: ', error);
+  }
+});
