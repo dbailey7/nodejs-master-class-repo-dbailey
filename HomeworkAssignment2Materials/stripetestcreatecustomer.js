c1370f885753a09a9e9b5c8f291e7b79e35ea514
dbailey7 <david.bailey@technologist.com>
2019-01-15 16:59:28 -0500

    Create stripetestcreatecustomer.js
    This is a test module for the stripe.com customer creation API.

---
+++ HomeworkAssignment2Materials/stripetestcreatecustomer.js
@@ -0,0 +1,20 @@
+/*
+ * This is a test module for the stripe.com customer creation API.  Enjoy watching!
+ */
+
+var stripe = require("stripe")("sk_test_UQemyl39wvtyhlN1ktMXobGY");
+
+stripe.customers.create({
+  metadata: {name: "Joseph Horatio Blowe"},
+  shipping.name: "Joseph Horatio Thadeus Blowe"
+  description: "Customer object for joe.blowe.7@gmail.com",
+  source: "tok_amex" // obtained with Stripe.js
+}, function(err, customer) {
+  // asynchronously called
+  if(!err, customer){
+    console.log('Success: ', customer.id, " ", customer.description, 'was created okay.');
+    console.log('Customer name: ', customer.shipping.name);
+  } else {
+    console.log('Error: customer creation failed; error was: ', err);
+  }
+});
