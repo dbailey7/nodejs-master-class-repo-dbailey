c77f4d53ae36b4c04892e2256ff62677253e7bf2
dbailey7 <david.bailey@technologist.com>
2019-01-15 18:44:43 -0500

    Update stripetestcreatecharge.js
    This is a test module for the stripe.com charge API.

--- HomeworkAssignment2Materials/stripetestcreatecharge.js
+++ HomeworkAssignment2Materials/stripetestcreatecharge.js
@@ -1,8 +1,9 @@
 /*
  * This is a test module for the stripe.com charge API.  Enjoy watching!
+ * To run this, you must replace the strings beginning with "YOUR-" with your own values.
  */
 
-var stripe = require("stripe")("sk_test_UQemyl39wvtyhlN1ktMXobGY");
+var stripe = require("stripe")("YOUR-API-KEY");
 
 stripe.charges.create({
   amount: 2000,
