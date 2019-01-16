9d33476b6f3256ebac528c2eb713725e2148f409
dbailey7 <david.bailey@technologist.com>
2019-01-16 09:25:14 -0500

    Update stripetestlistallskus.js

--- HomeworkAssignment2Materials/stripetestlistallskus.js
+++ HomeworkAssignment2Materials/stripetestlistallskus.js
@@ -1,8 +1,10 @@
 /*
  * This is a test module for the stripe.com list all orders API.  Enjoy watching!
+ * To run this, you must replace the strings beginning with "YOUR-API-KEY" with
+ * your own value.
  */
 
-var stripe = require("stripe")("sk_test_UQemyl39wvtyhlN1ktMXobGY");
+var stripe = require("stripe")("YOUR-API-KEY");
 
 stripe.orders.list(
   { limit: 20 },
